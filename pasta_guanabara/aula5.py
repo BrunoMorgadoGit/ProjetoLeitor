a9d07f2fdf7400f4f3bcd4c9f6cb7011de1a0fc1
Bruno Morgado <brunomorgadowork@gmail.com>
2026-01-26 13:16:51 -0300

    Delete pasta_guanabara directory

--- pasta_guanabara/aula5.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Fale um numero: '))
-n2 = int(input('Fale outro numero: '))
-resultado = n1 + n2
-print('A soma dos seus numeros é {}'.format(resultado))
--- pasta_guanabara/aula7.py
+++
@@ -1,3 +0,0 @@
-salario = float(input('Qual é seu salario? '))
-aumento = salario * 1.15
-print('Agora seu salario vai ser de {:.2f}'.format(aumento))
