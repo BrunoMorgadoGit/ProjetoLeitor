ae3f804fcb34200d6a6dded9ec9fe404c66e9c7c
Bruno Morgado <brunomorgadowork@gmail.com>
2026-01-26 13:17:12 -0300

    Delete cs50 directory

--- cs50/Exercicio1.py
+++
@@ -1,8 +0,0 @@
-#PERGUNTAR O NOME DA PESSOA E TRATAR A STR
-name = input(str('Qual é o seu nome? ')).strip().lower()
-
-#TRATAMENTO DA STR RECEBIDA
-name1 = name.title()
-
-#APRESENTAR O NOME DA PESSOA COM UM OLÁ
-print(f'Ola {name1}' + ", Obrigado por falar seu nome, burrao ")
